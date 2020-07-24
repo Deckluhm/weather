2069bb14e84cb1ebfd89dc9ba7f7993dbd4c5051
Deckluhm <arnaud.thomas-desessarts@live.com>
2020-07-24 23:05:48 +0200

    Define basic CSS defaults/reset

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,14 @@
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Inter&display=swap");
+
+html,
+body {
+  padding: 0;
+}
+
+body {
+  margin: 0;
+  font-family: Inter;
+}
 </style>
