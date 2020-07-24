04a57a2e4d002dccce482d5106f1eca459c6a277
Deckluhm <arnaud.thomas-desessarts@live.com>
2020-07-25 00:36:13 +0200

    Create splash screen

---
+++ src/views/Splash.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+#splash
+  img(src='@/assets/logo.svg')
+</template>
+
+<style lang="scss" scoped>
+#splash {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #5a9cfe;
+  img {
+    width: 100px;
+  }
+}
+</style>
