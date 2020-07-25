8619f4f427df37c3ebb33f5dae87ee5f0204cbcb
Deckluhm <arnaud.thomas-desessarts@live.com>
2020-07-25 12:58:03 +0200

    Install Font Awesome with Vue integration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import {} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+// library.add();
+
+Vue.component("icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
