b4f1d769b79209d92552af8b01de3b64aa22b4c2
Deckluhm <arnaud.thomas-desessarts@live.com>
2020-07-26 21:52:59 +0200

    Create cities list in store with associated mutations

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,17 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    cities: ["Lyon"] as Array<string>
+  },
+
+  mutations: {
+    addCity(state, city: string) {
+      state.cities.push(city);
+    },
+
+    removeCity(state, city: string) {
+      state.cities = state.cities.filter(city_ => city_ !== city);
+    }
+  }
 });
